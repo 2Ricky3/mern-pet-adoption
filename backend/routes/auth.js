1d98a3db3d2a76d9adc928290e95359b98a81797
Ricard <231197@virtualwindow.co.za>
2024-10-18 13:38:05 +0200

    update 1

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,25 +4,25 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
 
-// User signup route
-router.post('/signup', async (req, res) => {
-  const { username, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = new User({ username, password: hashedPassword });
-  await newUser.save();
-  res.status(201).json('User created successfully');
-});
-
-// User login route
 router.post('/login', async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  if (user && await bcrypt.compare(password, user.password)) {
-    const token = jwt.sign({ userId: user._id }, 'secretKey');
-    res.json({ token });
-  } else {
-    res.status(400).json('Invalid credentials');
-  }
-});
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user && await bcrypt.compare(password, user.password)) {
+      const token = jwt.sign({ userId: user._id }, 'secretKey', { expiresIn: '1h' });
+      res.json({ token });
+    } else {
+      res.status(400).json('Invalid credentials');
+    }
+  });
+  
+  router.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+    const token = jwt.sign({ userId: newUser._id }, 'secretKey', { expiresIn: '1h' });
+    res.status(201).json({ token });
+  });
+  
 
 module.exports = router;
